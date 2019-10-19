8e17c046ed50ed46c9d34fb4297299c8d59f3e99
ceko98 <ceko9898@gmail.com>
2019-10-20 02:41:22 +0300

    Fix color function

--- Assets/Scripts/WavelengthToRGB.cs
+++ Assets/Scripts/WavelengthToRGB.cs
@@ -1,53 +1,58 @@
 using UnityEngine;
 
-public class WaveToRGB {
+namespace Wavelength {
+    public class WaveToRGB {
     //Wavelength is in nanometers
-    static public Color waveToRGB(int wavelength) {
-        float attenuation;
-        float R, G, B;
-
-        if (wavelength >= 380 & wavelength <= 440) {
-            attenuation = 0f + 0f * (wavelength - 380) / (440 - 380);
-            R = ((-(wavelength - 440) / (440 - 380)) * attenuation);
-            G = 0f;
-            B = (1f * attenuation);
+        static public Color waveToRGB(int wavelength) {
+            float attenuation;
+            float R, G, B;
+            Debug.Log(wavelength);
+            if (wavelength >= 380f & wavelength <= 440f) {
+                attenuation = 0.3f + 0.7f * (wavelength - 380f) / (440f - 380f);
+                R = ((-(wavelength - 440f) / (440f - 380f)) * attenuation);
+                G = 0f;
+                B = (1f * attenuation);
+                Debug.Log(R + " " + G + " " + B);
+                
+            }
+            else if (wavelength >= 440f & wavelength <= 490f) {
+                R = 0f;
+                G = ((wavelength - 440f) / (490f - 440f));
+                B = 1f;
+                Debug.Log(R + " " + G + " " + B);
+            }
+            else if (wavelength >= 490f & wavelength <= 510f) {
+                R = 0f;
+                G = 1f;
+                B = (-(wavelength - 510f) / (510f - 490f));
+            }
+            else if (wavelength >= 510f & wavelength <= 580f) {
+                R = ((wavelength - 510f) / (580f - 510f));
+                G = 1f;
+                B = 0f;
+            }
+            else if (wavelength >= 580f & wavelength <= 645) {
+                R = 1f;
+                G = (-(wavelength - 645) / (645 - 580f));
+                B = 0f;
+            }
+            else if (wavelength >= 645 & wavelength <= 750f) {
+                attenuation = 0.3f + 0.7f * (750f - wavelength) / (750f - 645);
+                R = (1f * attenuation);
+                G = 0f;
+                B = 0f;
+            }
+            else {
+                R = 0f;
+                G = 0f;
+                B = 0f;
+            }
+            R = R * 255;
+            G = G * 255;
+            B = B * 255;
+            Color result = new Color(R,G,B);
+            Debug.Log("Light with wavelength "+wavelength+" has RGB color "+new Color(R,G,B).ToString());
+            return result;
         }
-        else if (wavelength >= 440 & wavelength <= 490) {
-            R = 0f;
-            G = ((wavelength - 440) / (490 - 440));
-            B = 1f;
-        }
-        else if (wavelength >= 490 & wavelength <= 510) {
-            R = 0f;
-            G = 1f;
-            B = (-(wavelength - 510) / (510 - 490));
-        }
-        else if (wavelength >= 510 & wavelength <= 580) {
-            R = ((wavelength - 510) / (580 - 510));
-            G = 1f;
-            B = 0f;
-        }
-        else if (wavelength >= 580 & wavelength <= 645) {
-            R = 1f;
-            G = (-(wavelength - 645) / (645 - 580));
-            B = 0f;
-        }
-        else if (wavelength >= 645 & wavelength <= 750) {
-            attenuation = 0f + 0f * (750 - wavelength) / (750 - 645);
-            R = (1f * attenuation);
-            G = 0f;
-            B = 0f;
-        }
-        else {
-            R = 0f;
-            G = 0f;
-            B = 0f;
-        }
-        R = R * 255;
-        G = G * 255;
-        B = B * 255;
-        Color result = new Color(R,G,B);
-        Debug.Log("Light with wavelength "+wavelength+" has RGB color "+new Color(R,G,B).ToString());
-        return result;
-    }   
+    }
 }
