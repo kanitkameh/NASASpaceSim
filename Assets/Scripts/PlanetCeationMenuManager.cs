c1156e7eda1f4a854872c61a7e374c38baf85e74
Kamen Vakavchiev <kanitkameh@gmail.com>
2019-10-19 15:24:38 +0300

    Add menu for creating planets

---
+++ Assets/Scripts/PlanetCeationMenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetCeationMenuManager : MonoBehaviour
+{   
+    bool isMenuOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMenuOpen=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public void click(){
+        Debug.Log("Planet Creation Menu");
+        if (isMenuOpen) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+        isMenuOpen = !isMenuOpen;
+    }
+    void openMenu(){
+        transform.position = transform.position + new Vector3(200,0,0);
+    }
+    void closeMenu(){
+        transform.position = transform.position + new Vector3(-200,0,0);
+    }
+}
