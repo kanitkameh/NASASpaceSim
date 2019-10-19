da6c6836e1d310c574174b94a8190612fecdb872
Kamen Vakavchiev <kanitkameh@gmail.com>
2019-10-19 18:48:39 +0300

    Add elliptical rotation script

---
+++ Assets/Scripts/EllipticalRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EllipticalRotation : MonoBehaviour
+{
+    public float scaleFactorX, scaleFactorY, centerX ,centerY, currentX, currentY; 
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     currentX =  centerX + (scaleFactorX * Mathf.Cos(Time.time));
+     currentY =  centerY + (scaleFactorY * Mathf.Sin(Time.time));
+     transform.position = new Vector3(currentX,currentY,0);
+    }
+}
